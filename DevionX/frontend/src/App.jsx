9267dd45896a3a26b9befc7f07f3768ac8d9aaa8
shivam prajapati <139427546+shivamp1111@users.noreply.github.com>
2025-01-04 18:12:20 +0530

    Update App.jsx

--- DevionX/frontend/src/App.jsx
+++ DevionX/frontend/src/App.jsx
@@ -1,6 +1,9 @@
 
-import './App.css'
-import Portfolio1 from './components/Portfolio1.jsx'
+import './App.css';
+import Portfolio1 from './components/Portfolio1.jsx';
+import About from './components/About.jsx'; // Ensure the case matches the file name
+import Para from './components/para/Para.jsx';
+import Card from './components/Card/Card.jsx';
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <div className='App'>
         {/* <Portfolio/> */}
         <Portfolio1/>
+        <About />
+        <Para/>
+        <Card/>
       </div>
     </>
   )
